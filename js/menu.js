0940591a75f204b097bc28b442952929976dd111
AKB <Ameliakingabujar2002@gmail.com>
2024-10-01 13:06:20 +0200

    Delete menu.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    console.log("Opening the nav");
-    document.getElementById("mymenu").style.width = "100%";
-  }
-  
-function closeNav() {
-    console.log("Closing the nav");
-    document.getElementById("mymenu").style.width = "0";
-  }
